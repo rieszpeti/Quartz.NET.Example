0775b6cb6e3f6bd1d37f814aefbd7d9d1bd2465b
SillySharp <rieszpeti@gmail.com>
2024-02-23 23:53:17 +0100

    format

--- Quartz.NET.Example/Startup.cs
+++ Quartz.NET.Example/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Quartz.Impl.AdoJobStore;
 using Quartz.NET.Example.JobHelpers;
@@ -7,13 +8,6 @@ using Quartz.NET.Example.Models;
 using Quartz.NET.Example.Quartz.Components;
 using Quartz.NET.Example.Repository;
 using Quartz.NET.Example.Services;
-using Quartz.Plugin.Interrupt;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Quartz.NET.Example;
 
@@ -23,125 +17,120 @@ internal static class Startup
         Host.CreateDefaultBuilder(args)
         .ConfigureServices((hostContext, services) =>
         {
-            services.Configure<QuartzOptions>(options =>
-            {
-                options.Scheduling.IgnoreDuplicates = true;         // default : false
-                options.Scheduling.OverWriteExistingData = true;    // default : true
-            });
-
-            // for setup
-            // https://www.quartz-scheduler.net/documentation/quartz-3.x/packages/microsoft-di-integration.html#di-aware-job-factories
-            services.AddQuartz(q =>
-            {
-
-                // You can setup this from Options
-                var schedName = q.SchedulerName = "SchedName";
-
-                // For clustering this is essential
-                q.SchedulerId = "SchedId";
-
-                q.UseJobFactory<JobFactory>();
-
-                // you can control whether job interruption happens for running jobs when scheduler is shutting down
-                q.InterruptJobsOnShutdown = true;
-
-                // when the program shuts down it will wait for the jobs to finish
-                q.InterruptJobsOnShutdownWithWait = true;
-
-                q.MaxBatchSize = 10;
-
-                // This is the default it is for auto-interrupt long running jobs
-                q.UseJobAutoInterrupt(options => options.DefaultMaxRunTime = TimeSpan.FromMinutes(5));
-
-                q.UseDefaultThreadPool(tp => tp.MaxConcurrency = 5);
-
-                q.UsePersistentStore(persistentStore =>
-                {
-                    persistentStore.UseSqlServer(sqlServerOptions =>
-                    {
-                        sqlServerOptions.UseDriverDelegate<SqlServerDelegate>();
-
-                        // You can get this also from options
-                        sqlServerOptions.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Quartz.NET.Example;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;";
-
-                        // This is the default it is for the quartz tables only
-                        sqlServerOptions.TablePrefix = "QRTZ_";
-                    });
-
-                    // This will check the TablePrefix from the UseSqlServer
-                    persistentStore.PerformSchemaValidation = true;
-
-                    persistentStore.UseProperties = true;
-
-                    persistentStore.UseNewtonsoftJsonSerializer();
-
-                    persistentStore.UseClustering();
-                });
-
-                services.AddQuartzHostedService(quartzOptions => 
-                {
-                    quartzOptions.AwaitApplicationStarted = true;
-
-                    // when the program shuts down it will wait for the jobs to finish
-                    quartzOptions.WaitForJobsToComplete = true;
-
-                    quartzOptions.StartDelay = TimeSpan.FromSeconds(1);
-                });
-
-                q.AddJobListener<JobListener>();
-#if DEBUG
-                var jobName = "TEST";
-                var triggerName = "TriggerName";
-
-                // Add job from program for test
-
-                //StartTestJob(q, typeof(DummyJob), jobName, triggerName);
-
-                //StartTestJob(q, typeof(SomeJob), jobName, triggerName);
-
-                StartTestJob(q, typeof(SomeJobWithNextJob), jobName, triggerName);
-#endif
-            });
+            // Configuration for Quartz
+            ConfigureQuartz(hostContext.Configuration, services);
 
             // Register services
-            services.AddDbContext<QuartzNetExampleContext>();
-
-            // Repository
-            services.AddTransient<Repo>();
-
-            // Jobs
-            services.AddTransient<DummyJob>();
-            services.AddTransient<ErrorJob>();
-            services.AddTransient<SomeJob>();
-
-            // JobHelpers
-            services.AddTransient<NextJobCreator>();
-
-            // Services
-            services.AddScoped<SomeService>();
-
-            // add because of migration
-            // Models
-            services.AddTransient<JobConfig>();
-            services.AddTransient<NextJobConfig>();
+            RegisterServices(services);
         });
+
+    private static void ConfigureQuartz(IConfiguration configuration, IServiceCollection services)
+    {
+        services.Configure<QuartzOptions>(options =>
+        {
+            options.Scheduling.IgnoreDuplicates = true;         // default : false
+            options.Scheduling.OverWriteExistingData = true;    // default : true
+        });
+
+        // for setup
+        // https://www.quartz-scheduler.net/documentation/quartz-3.x/packages/microsoft-di-integration.html#di-aware-job-factories
+        services.AddQuartz(q =>
+        {
+            ConfigureQuartzOptions(q);
+
+            ConfigurePersistentStore(q);
+
+            // Add test jobs if in debug mode
+#if DEBUG
+            var jobName = "TEST";
+            var triggerName = "TriggerName";
+            
+            StartTestJob(q, typeof(DummyJob), jobName, triggerName);
+
+            // StartTestJob(q, typeof(SomeJob), jobName, triggerName);
+
+            // StartTestJob(q, typeof(SomeJobWithNextJob), jobName, triggerName);
+#endif
+        });
+
+        services.AddQuartzHostedService(quartzOptions =>
+        {
+            quartzOptions.AwaitApplicationStarted = true;
+            // when the program shuts down it will wait for the jobs to finish
+            quartzOptions.WaitForJobsToComplete = true;
+            quartzOptions.StartDelay = TimeSpan.FromSeconds(1);
+        });
+    }
+
+    private static void ConfigureQuartzOptions(IServiceCollectionQuartzConfigurator q)
+    {
+        // You can setup this from Options
+        q.SchedulerName = "SchedName";
+
+        // For clustering this is essential
+        q.SchedulerId = "SchedId";
+
+        q.UseJobFactory<JobFactory>();
+        // Register job listeners
+        q.AddJobListener<JobListener>();
+        q.InterruptJobsOnShutdown = true;
+        q.InterruptJobsOnShutdownWithWait = true;
+        q.MaxBatchSize = 10;
+        q.UseJobAutoInterrupt(options => options.DefaultMaxRunTime = TimeSpan.FromMinutes(5));
+        q.UseDefaultThreadPool(tp => tp.MaxConcurrency = 5);
+    }
+
+    private static void ConfigurePersistentStore(IServiceCollectionQuartzConfigurator q)
+    {
+        q.UsePersistentStore(persistentStore =>
+        {
+            persistentStore.UseSqlServer(sqlServerOptions =>
+            {
+                sqlServerOptions.UseDriverDelegate<SqlServerDelegate>();
+                sqlServerOptions.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Quartz.NET.Example;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;";
+                sqlServerOptions.TablePrefix = "QRTZ_";
+            });
+
+            // This will check the TablePrefix from the UseSqlServer
+            persistentStore.PerformSchemaValidation = true;
+            persistentStore.UseProperties = true;
+            persistentStore.UseNewtonsoftJsonSerializer();
+            persistentStore.UseClustering();
+        });
+    }
 
     private static void StartTestJob(IServiceCollectionQuartzConfigurator q, Type jobType, string jobName, string triggerName)
     {
-        // Create key for the job
         var jobKey = new JobKey(jobName);
 
         q.AddJob(jobType, jobKey);
-
+        
         q.AddTrigger(t => t.ForJob(jobKey)
                            .WithIdentity(triggerName)
                            .StartNow());
+    }
 
-        //q.ScheduleJob<SomeJobWithNextJob>(
-        //    triggerConfigurator => triggerConfigurator
-        //        .WithIdentity(triggerName)
-        //        .StartNow(),
-        //    jobConfigurator => jobConfigurator
-        //        .WithIdentity(jobName));
+    private static void RegisterServices(IServiceCollection services)
+    {
+        services.AddDbContext<QuartzNetExampleContext>();
+
+        // Repository
+        services.AddTransient<Repo>();
+
+        // Jobs
+        services.AddTransient<DummyJob>();
+        services.AddTransient<ErrorJob>();
+        services.AddTransient<SomeJob>();
+
+        // JobHelpers
+        services.AddTransient<NextJobCreator>();
+
+        // Services
+        services.AddScoped<SomeService>();
+
+        // add because of migration
+        // Models
+        services.AddTransient<JobConfig>();
+        services.AddTransient<NextJobConfig>();
     }
 }
